cf6291b298f9cf9303d155cd93dce9abd6b9e6f3
Chan-Ho Kim <chanho.kim@outlook.com>
2015-02-26 11:07:36 -0600

    Starting

---
+++ src/PQueue.java
@@ -0,0 +1,28 @@
+
+public class PQueue {
+	
+	public PQueue(int m){
+		
+	}
+	
+	public int search(String name){
+		
+	}
+	
+	public String getFirst(){
+		
+	}
+
+	private class Node{
+		private String name;
+		private int priority;
+		
+		Node(String name, int priority){
+			this.name = name;
+			this.priority = priority;
+		}
+		
+		
+		
+	}
+}
